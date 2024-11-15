bcd9950d6261e0b7754979a64c291d6f4f6a7d8a
Nightjuanes <juanrodgon@unisabana.edu.co>
2024-11-15 08:31:11 -0500

    home
    se realizo home

--- SolarLinkApp/app/(tabs)/home/index.tsx
+++ SolarLinkApp/app/(tabs)/home/index.tsx
@@ -1,70 +1,46 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { Image, StyleSheet, View } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <Image
+        source={require('../../../assets/images/homeimage.png')}
+        style={styles.fullScreenImage}
+      />
+      <View style={styles.overlayContainer}>
+        <ThemedText style={[styles.text, { top: '10%', left: '15%' }]}>0.7kW HOME</ThemedText>
+        <ThemedText style={[styles.text, { top: '10%', right: '15%' }]}>5.4kW SOLAR</ThemedText>
+        <ThemedText style={[styles.text, { bottom: '10%', left: '15%' }]}>0kW GRID</ThemedText>
+        <ThemedText style={[styles.text, { bottom: '10%', right: '15%' }]}>4.7kW 98% BATTERY LEVEL</ThemedText>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
+  fullScreenImage: {
+    flex: 1,
+    width: '85%',
+    height: '75%',
     position: 'absolute',
+    resizeMode: 'cover',
+    marginHorizontal: '-7.5%', 
+    marginVertical: '15%',
+  },
+  overlayContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  text: {
+    position: 'absolute',
+    color: 'black',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });

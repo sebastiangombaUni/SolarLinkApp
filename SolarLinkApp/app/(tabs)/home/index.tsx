63ddf6d9f05229eba8dc0714ed814b1e7f1dbdb6
Nightjuanes <juanrodgon@unisabana.edu.co>
2024-11-16 19:15:21 -0500

    edits

--- SolarLinkApp/app/(tabs)/home/index.tsx
+++ SolarLinkApp/app/(tabs)/home/index.tsx
@@ -9,10 +9,18 @@ export default function HomeScreen() {
         style={styles.fullScreenImage}
       />
       <View style={styles.overlayContainer}>
-        <ThemedText style={[styles.text, { top: '10%', left: '15%' }]}>0.7kW HOME</ThemedText>
-        <ThemedText style={[styles.text, { top: '10%', right: '15%' }]}>5.4kW SOLAR</ThemedText>
-        <ThemedText style={[styles.text, { bottom: '10%', left: '15%' }]}>0kW GRID</ThemedText>
-        <ThemedText style={[styles.text, { bottom: '10%', right: '15%' }]}>4.7kW 98% BATTERY LEVEL</ThemedText>
+        <View style={[styles.line, styles.lineWindow]} />
+        <ThemedText style={[styles.text, { top: '10%', left: '10%' }]}>0.7kW HOME</ThemedText>
+        
+        <View style={[styles.line, styles.lineSolar]} />
+        <ThemedText style={[styles.text, { top: '10%', right: '25%' }]}>5.4kW SOLAR</ThemedText>
+        
+        <View style={[styles.line, styles.lineBattery]} />
+        <ThemedText style={[styles.text, { bottom: '6%', left: '40%' }]}>0kW GRID</ThemedText>
+        
+        <View style={[styles.line, styles.lineGrid]} />
+        <ThemedText style={[styles.text, { bottom: '6%', right: '20%' }]}>4.7kW</ThemedText>
+        <ThemedText style={[styles.text, { bottom: '4%', right: '20%' }]}>98% BL</ThemedText>
       </View>
     </View>
   );
@@ -42,5 +50,37 @@ const styles = StyleSheet.create({
     color: 'black',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  line: {
+    position: 'absolute',
+    width: 2,
+    backgroundColor: 'gray',
+  },
+  lineWindow: {
+    height: 190,
+    top: '3%',
+    left: '9%',
+    transform: [{ translateY: 50 }],
+ 
+    
+  },
+  lineSolar: {
+    height: 120,
+    top: '3%',
+    right: '46%',
+    transform: [{ translateY: 50 }],
+  },
+  lineBattery: {
+
+    height: 180,
+    bottom: '-2%',
+    right: '32%',
+    transform: [{ translateY: -50 }],
+  },
+  lineGrid: {
+    height: 130,
+    bottom: '-1%',
+    left: '39%',
+    transform: [{ translateY: -50 }],
   },
 });
